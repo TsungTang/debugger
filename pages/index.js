92c6384c0b8e2191b499150d034a36bad1dd2032
TsungTang <s710262101@gm.ntpu.edu.tw>
2021-10-06 23:57:22 +0800

    updated home page

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,11 @@ export default function Home() {
     <>
       <Carousel axis={"horizontal"} autoPlay={true} interval={3000} infiniteLoop={true} width="100%" dynamicHeight={false}>
 
-        <div className="carousel-container flex w-full items-center justify-start" style={{ backgroundImage: "url('/home_background1.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center", }}>
+        <div className="carousel-container flex w-full items-center justify-start" style={{ backgroundImage: "url('/homeBackground/home_background1.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center", }}>
           <div className="w-full xl:w-1/2 flex justify-center ">
-            <div className="flex flex-col items-start">
-              <h3 className="text-[50px] font-bold text-white ">Welcome to</h3>
-              <h1 className="text-[50px] font-bold text-white  mt-2 ">de Bugger</h1>
-
+            <div className=" w-[500px] flex flex-col items-start ">
+              <h3 className="text-[50px] text-left font-bold text-white">Welcome to
+                de Bugger</h3>
               <div className="mt-[50px]">
                 <Link href="/discover">
                   <button className=" text-xl font-bold bg-white text-green-primary px-11 py-4 rounded-full">Discover More Insects</button>
@@ -24,11 +23,11 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="carousel-container flex w-full items-center justify-start" style={{ backgroundImage: "url('/home_background2.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center" }}>
+        <div className="carousel-container flex w-full items-center justify-start" style={{ backgroundImage: "url('/homeBackground/home_background2.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center" }}>
           <div className="w-full xl:w-1/2 flex justify-center ">
-            <div className="flex flex-col items-start ">
-              <h3 className="text-[50px] font-bold text-white">Over 6,000 </h3>
-              <h1 className="text-[50px] font-bold text-white">different insects...</h1>
+            <div className=" w-[500px] flex flex-col items-start ">
+              <h3 className="text-[50px] text-left font-bold text-white">A place to see over 6,000 different insects’ future trends</h3>
+
               <div className="mt-[50px]">
                 <Link href="/discover">
                   <button className=" text-xl font-bold bg-white text-green-primary px-11 py-4 rounded-full">Discover More Insects</button>
@@ -37,11 +36,70 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="carousel-container flex w-full items-center justify-end" style={{ backgroundImage: "url('/home_background3.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center" }}>
+        <div className="carousel-container flex w-full items-center justify-end" style={{ backgroundImage: "url('/homeBackground/home_background3.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center" }}>
           <div className="w-full xl:w-1/2 flex justify-center ">
-            <div className="flex flex-col items-end">
-              <h3 className="text-[50px] font-bold text-white">Come and discover</h3>
-              <h1 className="text-[50px] font-bold text-white">Taiwan’s insects</h1>
+            <div className=" w-[500px] flex flex-col items-end">
+              <h3 className="text-[50px] font-bold text-right text-white">… so you can leverage it for other applications</h3>
+              <div className="mt-[50px]">
+                <Link href="/discover">
+                  <button className=" text-xl font-bold bg-white text-green-primary px-11 py-4 rounded-full">Discover More Insects</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-container flex w-full items-center justify-start" style={{ backgroundImage: "url('/homeBackground/home_background4.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center" }}>
+          <div className="w-full xl:w-1/2 flex justify-center ">
+            <div className=" w-[500px] flex flex-col items-start">
+              <h3 className="text-[50px] font-bold text-left text-white">You can see visualized biodistribution, trail, and biodiversity</h3>
+              <div className="mt-[50px]">
+                <Link href="/discover">
+                  <button className=" text-xl font-bold bg-white text-green-primary px-11 py-4 rounded-full">Discover More Insects</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-container flex w-full items-center justify-end" style={{ backgroundImage: "url('/homeBackground/home_background5.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center" }}>
+          <div className="w-full xl:w-1/2 flex justify-center ">
+            <div className=" w-[500px] flex flex-col items-end">
+              <h3 className="text-[50px] font-bold text-right text-white">… so you can leverage it for other applications</h3>
+              <div className="mt-[50px]">
+                <Link href="/discover">
+                  <button className=" text-xl font-bold bg-white text-green-primary px-11 py-4 rounded-full">Discover More Insects</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-container flex w-full items-center justify-end" style={{ backgroundImage: "url('/homeBackground/home_background6.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center" }}>
+          <div className="w-full xl:w-1/2 flex justify-center ">
+            <div className=" w-[500px] flex flex-col items-end">
+              <h3 className="text-[50px] font-bold text-right text-white">What’s more, based on the proposed CNN + Bio-vector framework …</h3>
+              <div className="mt-[50px]">
+                <Link href="/discover">
+                  <button className=" text-xl font-bold bg-white text-green-primary px-11 py-4 rounded-full">Discover More Insects</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-container flex w-full items-center justify-start" style={{ backgroundImage: "url('/homeBackground/home_background7.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center" }}>
+          <div className="w-full xl:w-1/2 flex justify-center ">
+            <div className=" w-[500px] flex flex-col items-start">
+              <h3 className="text-[50px] font-bold text-left text-white">… we predict insect’s distribution from now to the future(until 2022 Q3)!</h3>
+              <div className="mt-[50px]">
+                <Link href="/discover">
+                  <button className=" text-xl font-bold bg-white text-green-primary px-11 py-4 rounded-full">Discover More Insects</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-container flex w-full items-center justify-start" style={{ backgroundImage: "url('/homeBackground/home_background8.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center" }}>
+          <div className="w-full xl:w-1/2 flex justify-center ">
+            <div className=" w-[500px] flex flex-col items-start">
+              <h3 className="text-[50px] font-bold text-left text-white">So come discover Taiwan’s insects</h3>
               <div className="mt-[50px]">
                 <Link href="/discover">
                   <button className=" text-xl font-bold bg-white text-green-primary px-11 py-4 rounded-full">Discover More Insects</button>
